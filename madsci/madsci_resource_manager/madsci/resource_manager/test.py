827267715681d30f328a435c33512fb59fe0fa03
Dozgulbas <dozgulbas@anl.gov>
2025-01-23 19:39:11 -0800

    tests

--- madsci/madsci_resource_manager/madsci/resource_manager/test.py
+++ madsci/madsci_resource_manager/madsci/resource_manager/test.py
@@ -7,50 +7,46 @@ database_url = "postgresql://rpl:rpl@127.0.0.1:5432/resources"
 base_url = "http://localhost:8012" 
 client = ResourceClient(base_url=base_url, database_url=database_url)
 
-# message = "Hello, FastAPI!"
-# response = client.send_message(message)
-# print(response)
-# Create and add a stack
-# stack = Stack(
-#     resource_name="stack",
-#     resource_type="stack",
-#     capacity=10,
-#     ownership=None
-# )
-# stack = client.add_resource(stack)
+stack = Stack(
+    resource_name="stack",
+    resource_type="stack",
+    capacity=10,
+    ownership=None
+)
+stack = client.add_resource(stack)
 
-# # # Add assets to the stack and push them
-# for i in range(5):
-#     asset = Asset(resource_name=f"Test plate {i}",resource_type="asset")
-#     asset = client.add_resource(asset)
-#     client.push_to_stack(stack, asset)
+# # Add assets to the stack and push them
+for i in range(5):
+    asset = Asset(resource_name=f"Test plate {i}",resource_type="asset")
+    asset = client.add_resource(asset)
+    stack = client.push_to_stack(stack, asset)
 
-# # # # Retrieve the stack and pop two assets
-# retrieved_stack = client.get_resource(resource_id=stack.resource_id)
-# for _ in range(2):
-#     popped_asset, retrieved_stack = client.pop_from_stack(retrieved_stack)
-#     print(popped_asset)
+# # # Retrieve the stack and pop two assets
+retrieved_stack = client.get_resource(resource_id=stack.resource_id)
+for _ in range(2):
+    popped_asset, retrieved_stack = client.pop_from_stack(retrieved_stack)
+    print(popped_asset)
 
-# # # Create and add a queue
-# queue = Queue(
-#     resource_name="queue",
-#     resource_type="queue",
-#     capacity=10,
-#     ownership=None
-# )
-# queue = client.add_resource(queue)
+# # Create and add a queue
+queue = Queue(
+    resource_name="queue",
+    resource_type="queue",
+    capacity=10,
+    ownership=None
+)
+queue = client.add_resource(queue)
 
-# # Add assets to the queue and push them
-# for i in range(5):
-#     asset = Asset(resource_name=f"Test plate {i}", resource_type="asset")
-#     asset = client.add_resource(asset)
-#     client.push_to_queue(queue, asset)
+# Add assets to the queue and push them
+for i in range(5):
+    asset = Asset(resource_name=f"Test plate {i}", resource_type="asset")
+    asset = client.add_resource(asset)
+    queue = client.push_to_queue(queue, asset)
 
-# # Retrieve the queue and pop two assets, then push one back
-# retrieved_queue = client.get_resource(resource_id=queue.resource_id)
-# for _ in range(2):
-#     popped_asset,retrieved_queue = client.pop_from_queue(retrieved_queue)
-# client.push_to_queue(queue, popped_asset)
+# Retrieve the queue and pop two assets, then push one back
+retrieved_queue = client.get_resource(resource_id=queue.resource_id)
+for _ in range(2):
+    popped_asset,retrieved_queue = client.pop_from_queue(retrieved_queue)
+queue = client.push_to_queue(queue, popped_asset)
 
 # # # # Create and add a consumable resource
 consumable = Consumable(
@@ -60,7 +56,7 @@ consumable = Consumable(
     ownership=None,
     capacity=100,
 )
-# client.add_resource(consumable)
+consumable = client.add_resource(consumable)
 
 # # Create and add a pool resource with the consumable as a child
 pool = Pool(
@@ -69,22 +65,21 @@ pool = Pool(
     capacity=500.0,
     children={"Water": consumable}
 )
-print(pool)
 pool = client.add_resource(pool)
 
-# # # Perform operations on the pool
-# print(f"Initial Pool Quantity: {pool.quantity}")
-# client.increase_pool_quantity(pool, 50.0)
-# print(f"After Increase: {pool.quantity}")
+# # Perform operations on the pool
+print(f"Initial Pool Quantity: {pool.quantity}")
+pool = client.increase_pool_quantity(pool, 50.0)
+print(f"After Increase: {pool.quantity}")
 
-# client.decrease_pool_quantity(pool, 20.0)
-# print(f"After Decrease: {pool.quantity}")
+pool = client.decrease_pool_quantity(pool, 20.0)
+print(f"After Decrease: {pool.quantity}")
 
-# client.fill_pool(pool)
-# print(f"After Fill: {pool.quantity}")
+pool = client.fill_pool(pool)
+print(f"After Fill: {pool.quantity}")
 
-# client.empty_pool(pool)
-# print(f"After Empty: {pool.quantity}")
+pool = client.empty_pool(pool)
+print(f"After Empty: {pool.quantity}")
 
 # Add another pool resource
 pool1 = Pool(resource_name="Pool1", resource_type="pool", capacity=100, quantity=50)
@@ -97,15 +92,19 @@ plate = Collection(
     children={"A1": pool},
 )
 plate = client.add_resource(plate)
-print(plate.children)
+# print(plate.children)
 
 # Increase and decrease quantity in a specific well of the plate
-client.increase_plate_well(plate, "A1", 30)
+plate = client.increase_plate_well(plate, "A1", 30)
 print(f"A1 Quantity after increase: {plate.children}")
-
-client.decrease_plate_well(plate, "A1", 20)
+# plate = client.get_resource()
+plate = client.decrease_plate_well(plate, "A1", 20)
 print(f"A1 Quantity after decrease: {plate.children}")
 
 # Update or add a new well to the plate
-client.update_plate_well(plate, "A2", pool1)
+plate = client.update_collection_child(plate, "A2", pool1)
 print(f"A2 Pool Name: {plate.children}")
+
+pool.capacity = 1000
+updated_resource = client.update_resource(pool)
+print(updated_resource)
