7a672ffad5cd113f46c193fd46d4ec92a33aac1a
tginsbu1 <tsg44@cornell.edu>
2025-03-31 16:10:39 -0500

    fixing file arguments

--- src/madsci_squid/ui/src/components/LocationsPanel.vue
+++ src/madsci_squid/ui/src/components/LocationsPanel.vue
@@ -3,26 +3,20 @@
  <AddLocationModal v-model="add_modal" />
   <v-card class="pa-1 ma-1" col="3" title="Locations">
     <v-card-text>
-      <v-container v-if="Object.keys(workcell_state.locations).length > 0" fluid class="pa-1">
-        <v-row no-gutter wrap justify-content class="pa-1">
-          <v-col v-for="value, key in workcell_state.locations" class="pa-1" cols=12 sm=6 md=4 lg=3 xl=2>
-            <v-card-text @click="set_modal(value.location_name, value)">
-              <h3 wrap>{{ value.location_name }}</h3>
-              <p wrap class="text-caption">
-                {{ value.reservation }}
-              </p>
-              <p v-if="get_resource(resources, value)"> Occupied</p>
-              <p v-else> Empty</p>
-            </v-card-text>
-          </v-col>
-          <v-col  class="pa-1" cols=12 sm=6 md=4 lg=3 xl=2>
-            <v-card-text @click="add_modal = true">
-              <h2>+</h2>
-            </v-card-text>
-          </v-col>
-        </v-row>
-      </v-container>
-      <p v-else>No Locations Yet</p>
+      <v-data-table :headers="location_headers" hover
+      :items="location_items(workcell_state.locations, resources)"
+      no-data-text="No Locations" density="compact" :sort-by="sortBy" :hide-default-footer="location_items(workcell_state.locations, resources).length <= 10">
+      <template v-slot:item="{ item }: { item: any }">
+        <tr @click="set_modal(item.location_name, item)">
+          <td>{{ item.location_name }}</td>
+          <td>{{ get_resource(resources, item) }}</td>
+          <td>{{ item.location_id }}</td>
+          <td>{{ Object.keys(item.lookup) }}</td>
+        </tr>
+      </template>
+    </v-data-table>
+
+
     </v-card-text>
   </v-card>
 </template>
@@ -37,6 +31,35 @@ const modal_text = ref()
 const modal = ref(false)
 const add_modal = ref(false)
 import { resources, workcell_state } from "@/store";
+import { VDataTable } from 'vuetify/components';
+import { isTemplateElement } from '@babel/types';
+
+const sortBy: VDataTable['sortBy'] = [{ key: 'occupied', order: 'desc' }];
+const location_headers = [
+  { title: 'Name', key: 'location_name' },
+  { title: 'Occupied', key: 'occupied', sort: (a: string, b: string) => occupied_compare(a, b) },
+  { title: 'ID', key: 'location_id' },
+  { title: 'Nodes', key: 'nodes' },
+
+]
+
+function occupied_compare(a: string, b: string) {
+  if (a == "Occupied") {
+    return 1
+   } else if(a == "Empty") {
+     if (b == "Occupied") {
+      return -1
+
+     } else {
+      return 1
+     }
+
+
+   } else {
+    return -1
+   }
+}
+
 
 const set_modal = (title: string, value: Object) => {
   modal_title.value = title
@@ -48,14 +71,25 @@ function get_resource (resources: any, location: any) {
   if ("resource_id" in location && location.resource_id != null) {
    var resource = resources.find((element: any) => element.resource_id == location["resource_id"])
   } else {
-    return false
+    return "None"
   }
   if ("quantity" in resource) {
     if (resource.quantity > 0) {
-      return true
+      return "Occupied"
     }
-    return false
+    return "Empty"
   }
-  return false
+  return "None"
+}
+
+
+function location_items(locations: any, resources: any) {
+  var new_locations: any = []
+  Object.values(locations).forEach((item: any) => {
+    item["occupied"] = get_resource(resources, item);
+    new_locations.push(item);
+  })
+  return new_locations
+
 }
 </script>
--- src/madsci_squid/ui/src/store.ts
+++ src/madsci_squid/ui/src/store.ts
@@ -92,8 +92,8 @@ function get_status(value: any) {
     if(value["paused"] && value["paused"] != false) {
         return "paused"
     }
-    if(value["busy"] && value["busy"]) {
-        return "busy"
+    if((value["busy"] && value["busy"]) || (value["running_actions"] && (value["running_actions"].length > 0))) {
+        return "running"
     } else {
         return "ready"
     }
