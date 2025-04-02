db889146554218e834b17c9aa0b4899d47ab4415
tginsbu1 <tsg44@cornell.edu>
2025-04-02 01:18:19 -0500

    made it do the accordion thing

---
+++ src/madsci_squid/ui/src/components/ResourcesPanel.vue
@@ -0,0 +1,20 @@
+
+<template>
+<AddResourceModal v-model="add_modal" />
+<ResourceTable :resources=resources :parent_id="null" :hide_header="false"/>
+<v-btn @click="active_add()">Add Resource</v-btn>
+
+</template>
+
+
+<script setup lang="ts">
+
+import ResourceTable from './ResourceTable.vue';
+import AddResourceModal from './AddResourceModal.vue';
+import { ref } from 'vue';
+import { resources } from "@/store";
+const add_modal = ref(false)
+function active_add() {
+  add_modal.value=true
+}
+</script>
