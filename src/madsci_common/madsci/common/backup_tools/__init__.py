29fd16a6a5c8c2c482b4dc4a35a518d2bd06448b
Ryan Lewis <accounts+github@ryandlewis.dev>
2025-11-24 14:37:35 -0600

    WIP: standalone backup tooling phase 3

--- src/madsci_common/madsci/common/backup_tools/__init__.py
+++ src/madsci_common/madsci/common/backup_tools/__init__.py
@@ -3,5 +3,14 @@
 from .backup_manager import BackupManager
 from .backup_validator import BackupValidator
 from .base_backup import AbstractBackupTool, BackupInfo
+from .cli import main_mongodb_backup
+from .mongodb_backup import MongoDBBackupTool
 
-__all__ = ["AbstractBackupTool", "BackupInfo", "BackupManager", "BackupValidator"]
+__all__ = [
+    "AbstractBackupTool",
+    "BackupInfo",
+    "BackupManager",
+    "BackupValidator",
+    "MongoDBBackupTool",
+    "main_mongodb_backup",
+]
